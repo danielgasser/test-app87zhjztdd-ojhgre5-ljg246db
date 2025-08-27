c80c0f10bdf63bce0de2b678126690b983082cd9
Daniel Gasser <software@daniel-gasser.com>
2025-08-27 12:10:03 +0100

    fix

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -309,7 +309,7 @@ export default function MapScreen() {
           })}
         {(() => {
           return null;
-        })()}{" "}
+        })()}
         <DangerZoneOverlay
           dangerZones={dangerZones}
           visible={dangerZonesVisible}
