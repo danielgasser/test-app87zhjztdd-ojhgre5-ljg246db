2d4fadc1add630374d06696792a4bdac23cb8842
Daniel Gasser <software@daniel-gasser.com>
2025-10-21 18:14:19 +0200

    fix

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ module.exports = {
   expo: {
     name: "SafePath",
     slug: "safepath",
-    version: "0.9.1",
+    version: "1.6.0",
     scheme: "safepath",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
