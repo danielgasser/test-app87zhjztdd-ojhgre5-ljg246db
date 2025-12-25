3c011411475966c53ade12db8ad6af2326a205c7
Daniel Gasser <software@daniel-gasser.com>
2025-12-25 11:53:04 +0100

    fix

--- src/store/profileBannerSlice.ts
+++ src/store/profileBannerSlice.ts
@@ -1,9 +1,5 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import { APP_CONFIG } from '@/config/appConfig';
-import { logger } from '@/utils/logger';
-
-const BANNER_STORAGE_KEY = '@safepath_banner_dismissals';
 
 // Import banner types from config instead of defining enum
 export type BannerType = keyof typeof APP_CONFIG.PROFILE_COMPLETION.BANNERS.BANNER_TYPES;
