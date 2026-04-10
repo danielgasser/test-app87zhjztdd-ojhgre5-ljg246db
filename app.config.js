0f9e48c270a13b941519bb62bcd005024a57825e
Daniel Gasser <software@daniel-gasser.com>
2026-04-10 10:35:49 +0200

    fix

--- app.config.js
+++ app.config.js
@@ -5,7 +5,7 @@ module.exports = () => {
     expo: {
       name: appName,
       slug: "safepath",
-      version: "0.9.69",
+      version: "0.9.70",
       // increase build number!!!
       scheme: "safepath",
       orientation: "portrait",
@@ -17,7 +17,7 @@ module.exports = () => {
         backgroundColor: "#ffffff",
       },
       ios: {
-        buildNumber: "149",
+        buildNumber: "150",
         supportsTablet: false,
         bundleIdentifier: "com.keradaniel.safepath.app",
         associatedDomains: ["applinks:safepath.app"],
@@ -37,7 +37,7 @@ module.exports = () => {
         },
       },
       android: {
-        versionCode: 13,
+        versionCode: 14,
         minSdkVersion: 24, // Android 7.0
         targetSdkVersion: 34, // Android 14
         googleServicesFile:
